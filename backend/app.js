956977c8277f606b88209d4f4e4074ccfd89b0c4
devazarado <devazarado@gmail.com>
2024-12-15 22:08:25 -0300

    Section 2

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server on-line 2");
+});
+
 module.exports = app;
