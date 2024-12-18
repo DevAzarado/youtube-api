61f0019cbcbba2085845fa7cec2a55badf59d2cb
devazarado <devazarado@gmail.com>
2024-12-18 14:44:50 -0300

    Section 4

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_LOCAL_URI)
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    })
+    .catch((err) => {
+      console.log(`MongoDB, connection error: ${err.message}`);
+      process.exit(1);
+    });
+};
+
+module.exports = connectDatabase;
