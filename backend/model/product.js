58018f6e9426d6d8ae922fcc04b664261cbb564c
devazarado <devazarado@gmail.com>
2024-12-18 15:20:26 -0300

    Section 5

---
+++ backend/model/product.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLenght: [100, "Product name cannot exceed 100 characters"],
+  },
+  prive: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLenght: [5, "Product name cannot exceed 5 characters"],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please select category for this product"],
+    enum: {
+      values: [
+        "Electronics",
+        "Camera",
+        "laptops",
+        "Accessories",
+        "Headphones",
+        "Food",
+        "Books",
+        "Clothes/Shoes",
+        "Beauty/Healts",
+        "Sports",
+        "Outdoor",
+        "Home",
+      ],
+    },
+  },
+  seller: {
+    type: Number,
+    required: [true, "Please enter product seller"],
+  },
+  stocker: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLenght: [5, "Product name cannot exceed 5 characteres"],
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
